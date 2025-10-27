6e7a1c0b8e1d3ea72d005e4e094307bf25db3fad
shatha <sha7cs@gmail.com>
2025-10-27 10:29:38 +0300

    set up routes & and challenge list component done

---
+++ src/components/ChallengeList/ChallengeList.jsx
@@ -0,0 +1,43 @@
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+
+function ChallengeList() {
+    const [challengeList, setChallengeList] = useState(null) // not an empty array so that when its loading it displays 'loading' for user
+
+    async function getAllChallenges() {
+        const response = await axios.get(`http://127.0.0.1:8000/api/challenges/`)
+        setChallengeList(response.data)
+    }
+    useEffect(() => {
+        getAllChallenges()
+    }, [])
+
+    return (
+        <div>
+            <h1>All Challenges</h1>
+            <div className='challenges'>
+                {
+                    challengeList
+                        ?
+                        challengeList.length
+                            ?
+                            <div>
+                            {
+                                challengeList.map(challenge => {
+                                    return <p key={challenge.id}>challenge {challenge.name}</p>
+                                })
+                            }
+                            </div>
+                            :
+                            <h2>you have no challenges</h2>
+                        :
+                        <h2>Loading...</h2>
+                }
+
+            </div>
+        </div>
+    )
+}
+
+export default ChallengeList
