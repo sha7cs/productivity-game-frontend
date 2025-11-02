7b39c246750667bcfda6e5e9774b6a41e9b7119a
shatha <sha7cs@gmail.com>
2025-11-02 13:27:41 +0300

    challenge detail styling , with back button

--- src/components/ChallengeDetail/ChallengeDetail.jsx
+++ src/components/ChallengeDetail/ChallengeDetail.jsx
@@ -1,12 +1,17 @@
 import React, { use, useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import axios from 'axios'
-import { Link } from 'react-router'
+import { Link, useNavigate } from 'react-router'
 import { authRequest } from '../../lib/auth'
+import './challenge-detail.sass'
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { IoArrowBack } from "react-icons/io5";
 
 function ChallengeDetail() {
     const { challengeId } = useParams()
     const [errors, setErrors] = useState('')
+    const navigate = useNavigate();
 
     const [challenge, setChallenge] = useState({
         "members": [],
@@ -23,16 +28,29 @@ function ChallengeDetail() {
 
     async function getSingleChallenge() {
         try {
-            const response = await authRequest({method:'get',url:`http://127.0.0.1:8000/api/challenges/${challengeId}/`})
+            const response = await authRequest({ method: 'get', url: `http://127.0.0.1:8000/api/challenges/${challengeId}/` })
             setChallenge(response.data)
             return response.data
         } catch (error) {
             setErrors(error.response.data.error)
         }
     }
+
     useEffect(() => {
         getSingleChallenge()
     }, [])
+
+    function getDaysRemaining(startDate, endDate) {
+        const today = new Date();
+        const end = new Date(endDate);
+        const diffTime = end - today;
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); // convert ms to days
+        return diffDays > 0 ? diffDays : 0;
+    }
+    function handleGoalCheck(goalId) {
+        console.log('check a goal')
+    }
+
 
     if (errors) {
         return (
@@ -42,61 +60,101 @@ function ChallengeDetail() {
         )
     }
     return (
-        <div className='challenge-detail'>
+        <>
             {challenge.name != ""
                 ?
-                <div>
-                    <h1>{challenge.name}</h1>
-                    <Link to={`/challenges/${challengeId}/edit`}><button>edit</button></Link>
-                    <Link to={`/challenges/${challengeId}/confirm-delete`}><button style={{color:'red'}}>Delete</button></Link>                    
-                    <h3>{challenge.description}</h3>
-                    <h3>Join Code : {challenge.join_code}</h3>
-                    <h3>created by you</h3>
-                    <h3>started at: {challenge.start_date} and ends at {challenge.end_date}</h3>
-                    <h3>{challenge.winner == null ? "no winner yet" : challenge.winner}</h3>
-                    <h3>{challenge.is_active ? 'is active' : 'this challenge has ended'}</h3>
-                    {/* i need to make a component for a single goal? */}
-                    <h3> members: {
-                        challenge.members.length
-                            ?
-                            <ul>
+                <>
+                    <div className='challenge-detail-header'>
+                        <button onClick={() => navigate(-1)} className='back-btn'><IoArrowBack size={30}/></button>
+                        <h1>{challenge.name}</h1>
+                        <div className='actions'>
+                             {/* i might change them and make it ine button for edit and the delete is inside it  */}
+                            <Link to={`/challenges/${challengeId}/edit`}><button><FaEdit size={20}/></button></Link> 
+                            <Link to={`/challenges/${challengeId}/confirm-delete`}><button style={{ backgroundColor:'red', color: 'white' }}><MdDelete size={20} /></button></Link>
+                        </div>
+                    </div>
+                    <div className='challenge-detail-card'>
+                        {/* i need to make a component for a single goal? */}
+                        <div className='goals'>
+                            <h3>Goals</h3>
+                            {
+                                challenge.goals.length
+                                    ?
+                                    <ul>
+                                        {
+                                            challenge.goals.map(goal => {
+                                                return (
+                                                    <li key={`goal-${goal.id}`} className='goal-item'>
+                                                        <div className="goal-content">
+                                                            <div className='goal-header'>
+                                                                <span className="goal-title">{goal.title}</span>
+                                                                <Link to={`/challenges/${challenge.id}/edit-goal/${goal.id}`}><FaEdit size={20} color='#a6a6a6'/></Link>
+                                                            </div>
+                                                            <p className="goal-description">
+                                                                {goal.description}
+                                                            </p>
+                                                        </div>
+                                                        <input
+                                                            type="checkbox"
+                                                            className="goal-checkbox"
+                                                            onChange={() => handleGoalCheck(goal.id)}
+                                                        />
+
+                                                        {/* <Link to={`/challenges/${challenge.id}/delete-goal/${goal.id}`}><button style={{ color: 'red' }}>Delete</button></Link> */}
+                                                    </li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
+                                    :
+                                    'No assigned goals yet'
+                            }
+                            <Link to={`/challenges/${challenge.id}/add-goal`}><button className='add-goal'>Add Goal</button></Link>
+                        </div>
+
+                        <div className='challenge-details'>
+                            <div className='points'>
+                                <h3>Your points</h3>
+                                <h4>23</h4>
+                            </div>
+                            <div className='members'>
+                                <h3>Members Rank</h3>
                                 {
-                                    challenge.members.map(member => {
-                                        return <li key={`member-${member.id}`}>{member.user}</li>
-                                        // will see how to access the username instead of the id
-                                    })
+                                    challenge.members.length
+                                        ?
+                                        <ul>
+                                            {
+                                                challenge.members.map(member => {
+                                                    return <li key={`member-${member.id}`}>{member.user}</li>
+                                                    // will display them based on their rank
+                                                })
+                                            }
+                                        </ul>
+                                        :
+                                        'No assigned members yet'
                                 }
-                            </ul>
-                            :
-                            'No assigned members yet'
-                    }
-                    </h3>
-                    {/* i need to make a component for a single goal? */}
-                    <h3> goals: {
-                        challenge.goals.length
-                            ?
-                            <ul>
-                                {
-                                    challenge.goals.map(goal => {
-                                        return (
-                                            <li key={`goal-${goal.id}`}>
-                                                {goal.title}
-                                                <Link to={`/challenges/${challenge.id}/edit-goal/${goal.id}`}><button>edit</button></Link>
-                                                <Link to={`/challenges/${challenge.id}/delete-goal/${goal.id}`}><button style={{color:'red'}}>Delete</button></Link>
-                                            </li>
-                                        )
-                                    })
-                                }
-                            </ul>
-                            :
-                            'No assigned goals yet'
-                    }</h3>
-                    <Link to={`/challenges/${challenge.id}/add-goal`}><button>Add Goal</button></Link>
-                </div>
+                            </div>
+                            <div className='day-remaining'>
+                                <h3>Days Remaining</h3>
+                                <h3>⏰ {getDaysRemaining(challenge.start_date, challenge.end_date)} days ⏰</h3>
+                            </div>
+                            <a href="">History</a>
+                            {/* just for now i will make it hidden but i will create a modal for ot later */}
+                            <div className='challenge-info' hidden>
+                                <h3>{challenge.description}</h3>
+                                <h3>Join Code : {challenge.join_code}</h3>
+                                <h3>created by you</h3>
+                                <h3>{challenge.winner == null ? "no winner yet" : challenge.winner}</h3>
+                                <h3>{challenge.is_active ? 'is active' : 'this challenge has ended'}</h3>
+                            </div>
+                            {/* i need to make a component for a single goal? */}
+                        </div>
+                    </div>
+                </>
                 :
                 <h3>Loading ...</h3>
             }
-        </div>
+        </>
     )
 }
 
